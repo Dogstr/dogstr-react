b3195fe2008fce03de5cc7a5ff0703e4365957eb
erkartali <erkartali@gmail.com>
2021-02-15 12:54:32 -0600

    added axios as a dependency created map-container component wrote location lookup and dogpark lookup funtions

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,54 +1,54 @@
-import React, {useState, useEffect} from 'react';
-import { Redirect } from 'react-router';
-import { Button } from 'react-bootstrap';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+import MapContainer from "../subcomponents/MapContainer";
 
 const Dashboard = () => {
-
-        axios({
-            method: "GET",
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-            },
-            withCredentials: true,
-            url: "http://localhost:3000/checkAuthenticated"
-          }).then(function(res){
-            if (!res.data){
-                window.location.href = "/login"
-            }
-          })
-
-    const logOut = (e) => {
-        e.preventDefault()
-        axios({
-            method: "GET",
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-            },
-            withCredentials: true,
-            url: "http://localhost:3000/users/logout"
-          }).then(function(res){
-            window.location.reload()
-          })
+  axios({
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    withCredentials: true,
+    url: "http://localhost:3000/checkAuthenticated",
+  }).then(function (res) {
+    if (!res.data) {
+      window.location.href = "/login";
     }
+  });
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            let pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-            };
-            console.log(pos)
-        })
-    }
+  const logOut = (e) => {
+    e.preventDefault();
+    axios({
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      withCredentials: true,
+      url: "http://localhost:3000/users/logout",
+    }).then(function (res) {
+      window.location.reload();
+    });
+  };
 
-    return (
-        <div>
-            <h1>Home</h1>
-            <Button onClick={(e) => logOut(e)}>Logout</Button>
-        </div>
-    );
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      console.log(pos);
+    });
+  }
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <MapContainer />
+      <Button onClick={(e) => logOut(e)}>Logout</Button>
+    </div>
+  );
 };
 
 export default Dashboard;
